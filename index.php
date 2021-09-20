ee040c8865095fbc5a88c709b57994221ce464c4
madgear <anthony.monastrial@gmail.com>
2021-09-20 21:10:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-
+namespace php_sys\system;
+session_start();
+include_once "includes/config.php";
+include_once "includes/connections.php";
+include_once "includes/functions.php";
+$route = get_route();
 ?>
