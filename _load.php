f7726aad89d7f936c08b622c1413a80635449ab0
madgear <anthony.monastrial@gmail.com>
2022-04-17 09:06:19 +0800

    Add files via upload

---
+++ _load.php
@@ -0,0 +1,12 @@
+<?php
+namespace php_sys\system;
+if (!isset($RPATH)) {
+	$RPATH = "";
+	$ROOT_RPATH = ".";
+}
+
+include_once $RPATH . "includes/_cfg.php";
+include_once $RPATH . "includes/_conn.php";
+include_once $RPATH . "includes/_func.php";
+
+?>
