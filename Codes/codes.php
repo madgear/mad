286cde6fd0d005d39e4fe5d6e3acf7eaa1d70289
madgear <anthony.monastrial@gmail.com>
2023-06-06 20:10:49 +0800

    Update codes.php

--- Codes/codes.php
+++ Codes/codes.php
@@ -1,367 +1,157 @@
-
-
 <?php
-// Database credentials
-$servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database";
+// Assuming you have established a database connection
 
-// Create database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Parse the event data received from the AJAX request
+$eventData = $_POST['eventData'];
+$title = $eventData['title'];
+$start = $eventData['start'];
+$end = $eventData['end'];
+$recurringPattern = $eventData['recurringPattern'];
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Generate a list of specific dates based on the recurring pattern
+$dates = []; // Array to store the specific dates
+
+// Generate the specific dates based on the recurring pattern
+// Modify this logic based on your specific recurring pattern
+// For example, generating weekly occurrences for 4 weeks
+$startDate = new DateTime($start);
+$endDate = new DateTime($end);
+$interval = new DateInterval('P1W'); // Weekly interval
+$dateRange = new DatePeriod($startDate, $interval, $endDate);
+foreach ($dateRange as $date) {
+    $dates[] = $date->format('Y-m-d');
 }
 
-// Check the request type
-$requestType = $_SERVER['REQUEST_METHOD'];
-
-switch ($requestType) {
-    case 'GET':
-        // Fetch events from the database
-        $sql = "SELECT id, title, start_date, end_date FROM events";
-        $result = $conn->query($sql);
-
-        $events = array();
-
-        // Format events for FullCalendar
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $event = array();
-                $event['id'] = $row['id'];
-                $event['title'] = $row['title'];
-                $event['start'] = $row['start_date'];
-                $event['end'] = $row['end_date'];
-                $events[] = $event;
-            }
-        }
-
-        // Return events in JSON format
-        echo json_encode($events);
-        break;
-
-    case 'POST':
-        // Add a new event
-        $title = $_POST['title'];
-        $start = $_POST['start'];
-        $end = $_POST['end'];
-
-        $sql = "INSERT INTO events (title, start_date, end_date) VALUES ('$title', '$start', '$end')";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event added successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    case 'PUT':
-        // Update an existing event
-        parse_str(file_get_contents("php://input"), $putParams);
-        $id = $putParams['id'];
-        $title = $putParams['title'];
-        $start = $putParams['start'];
-        $end = $putParams['end'];
-
-        $sql = "UPDATE events SET title='$title', start_date='$start', end_date='$end' WHERE id=$id";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event updated successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    case 'DELETE':
-        // Delete an event
-        parse_str(file_get_contents("php://input"), $deleteParams);
-        $id = $deleteParams['id'];
-
-        $sql = "DELETE FROM events WHERE id=$id";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event deleted successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    default:
-        break;
+// Insert each occurrence as a separate entry in the events table
+foreach ($dates as $date) {
+    $query = "INSERT INTO events (title, start, end) VALUES ('$title', '$date $start', '$date $end')";
+    mysqli_query($connection, $query);
 }
 
-$conn->close();
-?>
+// Close the database connection
+mysqli_close($connection);
 
-<?php
-// Database credentials
-$servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database";
-
-// Create database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Check the request type
-$requestType = $_SERVER['REQUEST_METHOD'];
-
-switch ($requestType) {
-    case 'GET':
-        // Fetch events from the database
-        $sql = "SELECT id, title, start_date, end_date FROM events";
-        $result = $conn->query($sql);
-
-        $events = array();
-
-        // Format events for FullCalendar
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $event = array();
-                $event['id'] = $row['id'];
-                $event['title'] = $row['title'];
-                $event['start'] = $row['start_date'];
-                $event['end'] = $row['end_date'];
-                $events[] = $event;
-            }
-        }
-
-        // Return events in JSON format
-        echo json_encode($events);
-        break;
-
-    case 'POST':
-        // Add a new event
-        $title = $_POST['title'];
-        $start = $_POST['start'];
-        $end = $_POST['end'];
-        $roomId = $_POST['room_id'];
-
-        // Check if the room is available
-        $sql = "SELECT id FROM events WHERE room_id = $roomId AND ((start_date <= '$start' AND end_date >= '$start') OR (start_date <= '$end' AND end_date >= '$end'))";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            echo "Room not available at the selected time.";
-            exit;
-        }
-
-        // Insert the event into the database
-        $sql = "INSERT INTO events (title, start_date, end_date, room_id) VALUES ('$title', '$start', '$end', $roomId)";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event added successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    case 'PUT':
-        // Update an existing event
-        parse_str(file_get_contents("php://input"), $putParams);
-        $id = $putParams['id'];
-        $title = $putParams['title'];
-        $start = $putParams['start'];
-        $end = $putParams['end'];
-        $roomId = $putParams['room_id'];
-
-        // Check if the room is available
-        $sql = "SELECT id FROM events WHERE room_id = $roomId AND ((start_date <= '$start' AND end_date >= '$start') OR (start_date <= '$end' AND end_date >= '$end')) AND id != $id";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            echo "Room not available at the selected time.";
-            exit;
-        }
-
-        // Update the event in the database
-        $sql = "UPDATE events SET title='$title', start_date='$start', end_date='$end',
-
-room_id=$roomId WHERE id=$id";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event updated successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    case 'DELETE':
-        // Delete an event
-        parse_str(file_get_contents("php://input"), $deleteParams);
-        $id = $deleteParams['id'];
-
-        $sql = "DELETE FROM events WHERE id=$id";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event deleted successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    default:
-        break;
-}
-
-$conn->close();
+// Return a response to the AJAX request
+$response = array(
+    'success' => true,
+    'message' => 'Recurring events added successfully'
+);
+echo json_encode($response);
 ?>
 
 
 <?php
-// Database credentials
-$servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database";
+// Assuming you have established a database connection
 
-// Create database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Parse the event data received from the AJAX request
+$eventData = $_POST['eventData'];
+$eventId = $eventData['id'];
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Perform the necessary updates or deletions on the specific occurrence in the events table
+// Modify the following queries based on your requirements
+$updateQuery = "UPDATE events SET ... WHERE id = $eventId";
+$deleteQuery = "DELETE FROM events WHERE id = $eventId";
+
+// Execute the update or delete query based on the operation
+if ($eventData['action'] === 'update') {
+    mysqli_query($connection, $updateQuery);
+} elseif ($eventData['action'] === 'delete') {
+    mysqli_query($connection, $deleteQuery);
 }
 
-// Check the request type
-$requestType = $_SERVER['REQUEST_METHOD'];
+// Close the database connection
+mysqli_close($connection);
 
-switch ($requestType) {
-    case 'GET':
-        // Fetch events from the database
-        $sql = "SELECT id, title, start_date, end_date, room_id, teacher_id FROM events";
-        $result = $conn->query($sql);
-
-        $events = array();
-
-        // Format events for FullCalendar
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $event = array();
-                $event['id'] = $row['id'];
-                $event['title'] = $row['title'];
-                $event['start'] = $row['start_date'];
-                $event['end'] = $row['end_date'];
-                $event['room_id'] = $row['room_id'];
-                $event['teacher_id'] = $row['teacher_id'];
-                $events[] = $event;
-            }
-        }
-
-        // Return events in JSON format
-        echo json_encode($events);
-        break;
-
-    case 'POST':
-        // Add a new event
-        $title = $_POST['title'];
-        $start = $_POST['start'];
-        $end = $_POST['end'];
-        $roomId = $_POST['room_id'];
-        $teacherId = $_POST['teacher_id'];
-
-        // Check if the room is available
-        $roomSql = "SELECT id FROM events WHERE room_id = $roomId AND ((start_date <= '$start' AND end_date >= '$start') OR (start_date <= '$end' AND end_date >= '$end'))";
-        $roomResult = $conn->query($roomSql);
-
-        if ($roomResult->num_rows > 0) {
-            echo "Room not available at the selected time.";
-            exit;
-        }
-
-        // Check if the teacher is available
-        $teacherSql = "SELECT id FROM events WHERE teacher_id = $teacherId AND ((start_date <= '$start' AND end_date >= '$start') OR (start_date <= '$end' AND end_date >= '$end'))";
-        $teacherResult = $conn->query($teacherSql);
-
-        if ($teacherResult->num_rows > 0) {
-            echo "Teacher not available at the selected time.";
-            exit;
-        }
-
-        // Insert the event into the database
-        $sql = "INSERT INTO events (title, start_date, end_date, room_id, teacher_id) VALUES ('$title', '$start', '$end', $roomId, $teacherId)";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event added successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    case 'PUT':
-        // Update an existing event
-        parse_str(file_get_contents("php://input"), $putParams);
-        $id = $putParams['id'];
-        $title = $putParams['title'];
-        $start =
-
-
-$putParams['start'];
-        $end = $putParams['end'];
-        $roomId = $putParams['room_id'];
-        $teacherId = $putParams['teacher_id'];
-
-        // Check if the room is available
-        $roomSql = "SELECT id FROM events WHERE room_id = $roomId AND ((start_date <= '$start' AND end_date >= '$start') OR (start_date <= '$end' AND end_date >= '$end')) AND id != $id";
-        $roomResult = $conn->query($roomSql);
-
-        if ($roomResult->num_rows > 0) {
-            echo "Room not available at the selected time.";
-            exit;
-        }
-
-        // Check if the teacher is available
-        $teacherSql = "SELECT id FROM events WHERE teacher_id = $teacherId AND ((start_date <= '$start' AND end_date >= '$start') OR (start_date <= '$end' AND end_date >= '$end')) AND id != $id";
-        $teacherResult = $conn->query($teacherSql);
-
-        if ($teacherResult->num_rows > 0) {
-            echo "Teacher not available at the selected time.";
-            exit;
-        }
-
-        // Update the event in the database
-        $sql = "UPDATE events SET title='$title', start_date='$start', end_date='$end', room_id=$roomId, teacher_id=$teacherId WHERE id=$id";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event updated successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    case 'DELETE':
-        // Delete an event
-        parse_str(file_get_contents("php://input"), $deleteParams);
-        $id = $deleteParams['id'];
-
-        $sql = "DELETE FROM events WHERE id=$id";
-        $result = $conn->query($sql);
-
-        if ($result === TRUE) {
-            echo "Event deleted successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-        break;
-
-    default:
-        break;
-}
-
-$conn->close();
+// Return a response to the AJAX request
+$response = array(
+    'success' => true,
+    'message' => 'Event updated/deleted successfully'
+);
+echo json_encode($response);
 ?>
+
+
+// Assuming you have initialized FullCalendar and have the necessary event handlers
+
+// Handle the creation of a recurring event
+function handleRecurringEventCreation(eventData) {
+  // Parse the event data and generate the list of specific dates
+  const { title, start, end, recurringPattern } = eventData;
+  const dates = generateSpecificDates(start, end, recurringPattern);
+
+  // Send AJAX requests to insert each occurrence as a separate entry
+  dates.forEach(date => {
+    const eventData = { title, start: date + ' ' + start, end: date + ' ' + end };
+    createEvent(eventData);
+  });
+}
+
+// Function to generate specific dates based on the recurring pattern
+function generateSpecificDates(start, end, recurringPattern) {
+  // Modify this logic based on your specific recurring pattern
+  // For example, generating weekly occurrences for 4 weeks
+  const startDate = moment(start);
+  const endDate = moment(end);
+  const dates = [];
+
+  let currentDate = startDate.clone();
+  while (currentDate.isSameOrBefore(endDate)) {
+    dates.push(currentDate.format('YYYY-MM-DD'));
+    currentDate.add(1, 'week'); // Weekly interval
+  }
+
+  return dates;
+}
+
+// Function to send AJAX request to create an event
+function createEvent(eventData) {
+  $.ajax({
+    url: 'create-event.php',
+    method: 'POST',
+    data: { eventData },
+    dataType: 'json',
+    success: function(response) {
+      // Handle the success response
+      console.log(response.message);
+    },
+    error: function(xhr, status, error) {
+      // Handle the error response
+      console.error(error);
+    }
+  });
+}
+
+// Attach the handler to the appropriate FullCalendar event creation method
+calendar.addEvent = handleRecurringEventCreation;
+
+
+
+// Assuming you have initialized FullCalendar and have the necessary event handlers
+
+// Handle the update or deletion of an individual occurrence
+function handleIndividualOccurrenceUpdate(eventData) {
+  const { id, action } = eventData;
+
+  // Send AJAX request to update or delete the individual occurrence
+  $.ajax({
+    url: 'update-event.php',
+    method: 'POST',
+    data: { eventData },
+    dataType: 'json',
+    success: function(response) {
+      // Handle the success response
+      console.log(response.message);
+      if (action === 'delete') {
+        // Remove the event from the calendar if it was deleted
+        calendar.getEventById(id).remove();
+      }
+    },
+    error: function(xhr, status, error) {
+      // Handle the error response
+      console.error(error);
+    }
+  });
+}
+
+// Attach the handler to the appropriate FullCalendar event update or deletion method
+calendar.eventClick = handleIndividualOccurrenceUpdate;
