c07b9392390a3edef15d75b891a3e96516387616
madgear <anthony.monastrial@gmail.com>
2023-06-11 19:04:26 +0800

    Update cal.php

--- Codes/cal.php
+++ Codes/cal.php
@@ -67,3 +67,71 @@ $(document).ready(function() {
     });
 });
 </script>
+
+
+<script>
+$(document).ready(function() {
+    // Populate the select box with available rooms
+    $.ajax({
+        url: 'get_available_rooms.php',
+        type: 'GET',
+        success: function(response) {
+            var rooms = response.rooms;
+            var roomSelect = $('#room-select');
+
+            // Add options to the select box
+            $.each(rooms, function(index, room) {
+                roomSelect.append($('<option></option>').val(room).text(room));
+            });
+        },
+        error: function() {
+            alert('Failed to fetch available rooms');
+        }
+    });
+
+    // Populate the select box with available teachers
+    $.ajax({
+        url: 'get_available_teachers.php',
+        type: 'GET',
+        success: function(response) {
+            var teachers = response.teachers;
+            var teacherSelect = $('#teacher-select');
+
+            // Add options to the select box
+            $.each(teachers, function(index, teacher) {
+                teacherSelect.append($('<option></option>').val(teacher).text(teacher));
+            });
+        },
+        error: function() {
+            alert('Failed to fetch available teachers');
+        }
+    });
+
+    $('#calendar').fullCalendar({
+        // Set your calendar options here
+        // ...
+
+        // Handle event creation
+        select: function(start, end) {
+            // Use the selected room and teacher from the select boxes
+            var room = $('#room-select').val();
+            var teacher = $('#teacher-select').val();
+
+            if (room && teacher) {
+                // ...
+            } else {
+                alert('Please select a room and teacher');
+            }
+        }
+    });
+});
+</script>
+
+<select id="room-select">
+    <option value="">Select Room</option>
+</select>
+
+<select id="teacher-select">
+    <option value="">Select Teacher</option>
+</select>
+
